4a044e40ce74f45e29fd138ae7d1f397cc2c4d8a
EA-Emerson <emokiniovoaghogho@gmail.com>
2024-07-13 23:01:46 +0100

    removed all warnings

--- src/components/MoreProducts.jsx
+++ src/components/MoreProducts.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 // import placeholderImage from "../assets/placeholder-image.png"; // Import a placeholder image for products without specific images
 
 const MoreProducts = ({ products }) => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import Explore from "./Explore";
 import flower from "../assets/black flower vase.png";
 import fatLight from "../assets/fat lights.png";
