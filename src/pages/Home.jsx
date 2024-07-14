6f7cea928b5c5a85505feae946dee851ee2ebe46
Aghogho Emokiniovo <emokiniovoaghogho@gmail.com>
2024-07-14 04:02:54 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import weirdStatue from "../assets/weird statues.png";
 import curtain from "../assets/curtain cate.png";
 
 const Home = ({ products }) => {
-  const productCards = products.slice(0, 4);
+  const productCards = products.slice(0, 9);
 
   return (
     <div>
