043662a83731b0d367e2ac2b0f193ce4db85cd58
Aghogho Emokiniovo <emokiniovoaghogho@gmail.com>
2024-07-14 04:41:27 +0100

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -28,7 +28,7 @@ const Products = ({ products, addToCart }) => {
           Filter <img className="mt-1 w-5 h-5" src={filter} alt="filter" />
         </button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8 my-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8 my-8">
         {products.map((product) => (
           <div
             key={product.id}
