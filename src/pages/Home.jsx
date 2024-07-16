4b0a9a3109d96a0676b106077b8f27eb01d5d5ed
EA-Emerson <emokiniovoaghogho@gmail.com>
2024-07-17 00:05:55 +0100

    blah

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Explore from "./Explore";
 import flower from "../assets/black flower vase.png";
